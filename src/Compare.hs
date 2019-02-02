e213ff67277e0349f5feb7eb4ace3a5929187d3b
Inanna Malick <inanna@recursion.wtf>
2019-02-01 18:43:00 -0800

    use standard Term, rejigger types to make it work. more expressive?

--- src/Compare.hs
+++ src/Compare.hs
@@ -19,14 +19,15 @@ import           Types
 --------------------------------------------
 
 -- | fetch a value from the global store. Pretend this involves a network call.
-deref :: GlobalStore -> Pointer -> IO (Term (NamedEntity Tree))
+-- NOTE: could also encode at the type level that this only returns one layer via return type..
+deref :: GlobalStore -> Pointer -> IO ConcreteMerkleTreeLayer
 deref store p = do
   globalStateStore <- readIORef store
   putStrLn $ "attempt to deref " ++ show p ++ " via global state store"
   case Map.lookup p globalStateStore of
     Nothing -> error "YOLO420"
     Just x  -> do
-      putStrLn $ "returning deref res: " ++ showT x
+      -- putStrLn $ "returning deref res: " ++ showT x
       pure x
 
 
@@ -34,23 +35,27 @@ deref store p = do
 --   lazily fetches structure of the two trees such that only the parts required
 --   to do this comparison are fetched from the global state store (via 'network call')
 -- TODO: hammer this into shape such that I can use bare-bones recursion schemes such as 'cata'
-compareMerkleTrees :: GlobalStore -> HashTerm (NamedEntity Tree) -> HashTerm (NamedEntity Tree) -> IO [Diff]
+compareMerkleTrees
+  :: GlobalStore
+  -> MerkleTree  -> MerkleTree
+  -> IO [Diff]
 compareMerkleTrees store ht1 ht2 = do
   -- putStrLn $ "compareMerkleTrees: " ++ showHT ht1 ++ ", " ++ showHT ht2
   case (htPointer ht1 == htPointer ht2) of
     True  -> pure [] -- no need to explore further here
     False -> do -- hash mismatch - deref and explore further
-      ne1 <- out <$> derefOneLayer ht1
-      ne2 <- out <$> derefOneLayer ht2
+      ne1 <- derefOneLayer ht1
+      ne2 <- derefOneLayer ht2
       compareDerefed ne1 ne2
 
   where
 
 
     compareDerefed ne1 ne2 = do
-      putStrLn $ "compareDerefed: " ++ showTree ne1 ++ ", " ++ showTree ne2
+      -- putStrLn $ "compareDerefed: " ++ showTree ne1 ++ ", " ++ showTree ne2
       compareDerefed' ne1 ne2
 
+    compareDerefed' :: ConcreteMerkleTreeLayer -> ConcreteMerkleTreeLayer -> IO [Diff]
     compareDerefed' (NamedEntity name1 entity1) (NamedEntity name2 entity2)
       | name1 /= name2 =
           -- flatten out sub-entities to only contain pointers then check equality
@@ -79,26 +84,27 @@ compareMerkleTrees store ht1 ht2 = do
               derefedNs1 <- traverse derefOneLayer filteredNs1
               derefedNs2 <- traverse derefOneLayer filteredNs2
 
-              let mkByNameMap :: [Term (NamedEntity Tree)] -> HashMap Name (Term (NamedEntity Tree))
+              let mkByNameMap :: [ConcreteMerkleTreeLayer] -> HashMap Name ConcreteMerkleTreeLayer
                   mkByNameMap ns = Map.fromList $ fmap mkByNameMapEntry ns
-                  mkByNameMapEntry t@(In (NamedEntity n _)) = (n, t)
+                  mkByNameMapEntry :: ConcreteMerkleTreeLayer -> (Name, ConcreteMerkleTreeLayer)
+                  mkByNameMapEntry t@(NamedEntity n _) = (n, t)
 
                   byNameMap1 = mkByNameMap derefedNs1
                   byNameMap2 = mkByNameMap derefedNs2
 
-              let cmpRes :: [These (Name, Term (NamedEntity Tree))]
+              let cmpRes :: [These (Name, ConcreteMerkleTreeLayer)]
                   cmpRes = mapCompare byNameMap1 byNameMap2
 
               join <$> traverse resolveMapDiff cmpRes
 
-    resolveMapDiff :: These (Name, Term (NamedEntity Tree))
+    resolveMapDiff :: These (Name, ConcreteMerkleTreeLayer)
                    -> IO [Diff]
     resolveMapDiff (This (name,_))     = pure [EntityDeleted name]
     resolveMapDiff (That (name,_))     = pure [EntityCreated name]
-    resolveMapDiff (These (_,a) (_,b)) = compareDerefed (out a) (out b)
+    resolveMapDiff (These (_,a) (_,b)) = compareDerefed a b
 
-    derefOneLayer :: HashTerm (NamedEntity Tree) -> IO (Term (NamedEntity Tree))
-    derefOneLayer ht = case ht of
+    derefOneLayer :: MerkleTree -> IO ConcreteMerkleTreeLayer
+    derefOneLayer ht = case out ht of
       Direct _ t -> pure t
       Indirect p -> do
         t <- deref store p
--- src/Ingress.hs
+++ src/Ingress.hs
@@ -17,7 +17,7 @@ import Types
 
 -- | actual dir recursive traversal
 -- ignores permissions in diffs and coerces file contents into unicode #YOLO
-buildDirTree :: GlobalStore -> FilePath -> FilePath -> IO (HashTerm (NamedEntity Tree))
+buildDirTree :: GlobalStore -> FilePath -> FilePath -> IO MerkleTree
 buildDirTree store path fn = do
   let fullpath = path ++ "/" ++ fn
   -- putStrLn $ "run buildDirTree on: " ++ fullpath
@@ -27,7 +27,7 @@ buildDirTree store path fn = do
       fc <- readFile fullpath
       let (pointer, entity) = lift $ NamedEntity fn $ Leaf fc
       modifyIORef' store (Map.insert pointer entity)
-      pure $ Direct pointer entity
+      pure $ In $ Direct pointer entity
     else do
       isDir <- Dir.doesDirectoryExist fullpath
       if isDir
@@ -36,5 +36,5 @@ buildDirTree store path fn = do
           entities <- traverse (buildDirTree store fullpath) contents
           let (pointer, entity) = lift $ NamedEntity fn $ Node $ fmap htPointer entities
           modifyIORef' store (Map.insert pointer entity)
-          pure $ Direct pointer entity
+          pure $ In $ Direct pointer entity
       else error $ "invalid file path " ++ fullpath ++ "? wtf yo no symlinks or sockets allowed"
