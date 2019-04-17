3ca50d3bcc77c529474c9aec1b3a719f1005e5a6
Inanna Malick <inanna@recursion.wtf>
2019-04-16 22:38:34 -0700

    update readme, add blockchain example to tests

--- test/Merkle/Store/InMemoryStoreSpec.hs
+++ test/Merkle/Store/InMemoryStoreSpec.hs
@@ -14,10 +14,16 @@ import           Merkle.Store.InMemory
 --------------------------------------------
 
 spec :: Spec
-spec = describe "pure in-memory store" $ do
-  it "round trips recursive dir tree structures with max depth 5" $ do
+spec = describe "in-memory store (IORef backed)" $ do
+  let testHarness g = do
+        -- use one shared hash-addressed store for all tests
+        -- if hash == , then content ==, so safe. Merkle!
+        ior <- newIORef M.empty
+        requireProperty $ storeTestDeep g $ inMemoryStore ior
 
-    -- use one shared hash-addressed store for all tests
-    -- if hash == , then content ==, so safe. Merkle!
-    ior <- newIORef M.empty
-    requireProperty $ storeTestDeep (genMockDirTree 5) $ inMemoryStore ior
+  it "round trips recursive dir tree structures with max depth 5" $
+    testHarness $ genMockDirTree 5
+
+
+  it "round trips recursive mock blockchain structures with constant depth 25" $
+    testHarness $ genMockBlockchain 25
--- test/Merkle/Store/NetStoreSpec.hs
+++ test/Merkle/Store/NetStoreSpec.hs
@@ -6,6 +6,7 @@ module Merkle.Store.NetStoreSpec where
 import qualified Control.Concurrent               as C
 import           Control.Exception.Safe (throw, bracket)
 import           Control.Monad.IO.Class
+import qualified Data.Aeson as AE
 import           Network.HTTP.Client (newManager, defaultManagerSettings)
 import qualified Network.Wai.Handler.Warp         as Warp
 import           Servant.Client (mkClientEnv, runClientM, Scheme(..), BaseUrl(..))
@@ -20,25 +21,32 @@ import           Merkle.Store
 import           Merkle.Store.FileSystem
 import           Merkle.Store.Network
 import           Merkle.Store.TestUtils
+import           Merkle.Types
+import           Util.RecursionSchemes
 --------------------------------------------
 
 
 spec :: Spec
-spec = describe "network store" $ do
-  it "round trips recursive dir tree structures with max depth 5" $ do
+spec = describe "network store served over REST, backed by a file system store" $ do
+  let testHarness :: forall f
+                   . (AE.ToJSON1 f, AE.FromJSON1 f, Show1 f, Hashable f, Eq1 f, Traversable f)
+                  => Gen (Fix f) -> IO ()
+      testHarness g = do
+        -- use one shared hash-addressed store for all tests
+        -- if hash == , then content ==, so safe. Merkle!
+        withSystemTempDirectory "proptest" $ \ netpath -> do
+          let port = 8081 -- todo random?
+          manager <- newManager defaultManagerSettings
+          let env = mkClientEnv manager (BaseUrl Http "localhost" port "")
+          let netStore' = liftStore (\mx -> liftIO (runClientM mx env) >>= either throw pure) netStore
+          bracket (liftIO . C.forkIO . Warp.run port $ app (fsStore netpath :: Store IO f))
+            C.killThread $ \_ -> do
+              -- all this should work fine in parallel
+              -- store is hash addressed, so no collisions should matter
+              requireProperty $ storeTestDeep g netStore'
 
-    -- use one shared hash-addressed store for all tests
-    -- if hash == , then content ==, so safe. Merkle!
-    withSystemTempDirectory "proptest" $ \ fspath ->
-          requireProperty $ storeTestDeep (genMockDirTree 5) $ fsStore fspath
-    withSystemTempDirectory "proptest" $ \ netpath -> do
-      let port = 8081 -- todo random?
-      manager <- newManager defaultManagerSettings
-      let env = mkClientEnv manager (BaseUrl Http "localhost" port "")
-      let netStore' :: Store (PropertyT IO) MockDirectoryTree
-          netStore' = liftStore (\mx -> liftIO (runClientM mx env) >>= either throw pure) netStore
-      bracket (liftIO . C.forkIO . Warp.run port $ app (fsStore netpath :: Store IO MockDirectoryTree))
-        C.killThread $ \_ -> do
-          -- all this should work fine in parallel
-          -- store is hash addressed, so no collisions should matter
-          requireProperty $ storeTestDeep (genMockDirTree 5) netStore'
+  it "round trips recursive dir tree structures with max depth 5" $
+    testHarness $ genMockDirTree 5
+
+  it "round trips recursive mock blockchain structures with constant depth 25" $
+    testHarness $ genMockBlockchain 25
