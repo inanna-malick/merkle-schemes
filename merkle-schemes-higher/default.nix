9e812fc931c5415e345849c059cf8d103967e6ea
Inanna Malick <inanna@recursion.wtf>
2019-05-15 11:29:47 -0700

    tweak nix

--- merkle-schemes-higher/default.nix
+++ merkle-schemes-higher/default.nix
@@ -8,9 +8,9 @@ mkDerivation {
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [
-    aeson base base16-bytestring base64-bytestring bifunctors
-    bytestring containers deriving-compat directory free memory mtl
-    pretty random safe-exceptions singletons text transformers
+    aeson base base64-bytestring
+    bytestring containers deriving-compat free
+    random safe-exceptions singletons text
     unordered-containers
   ];
   libraryToolDepends = [ hpack ];
