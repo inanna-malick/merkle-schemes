a7c04ab012fc3f3e3ee72b03919a7496baf42ff1
Inanna Malick <inanna@recursion.wtf>
2019-05-16 13:45:42 -0700

    inherit pkgs

--- default.nix
+++ default.nix
@@ -1,22 +1,13 @@
+{ pkgs ? import <nixpkgs> {}
+}:
+
 let
-
-
-  config = {
-    packageOverrides = pkgs: rec {
-      haskellPackages = pkgs.haskellPackages.override {
-        overrides = haskellPackagesNew: haskellPackagesOld: rec {
-          merkle-schemes-higher = haskellPackagesOld.callPackage ./merkle-schemes-higher/default.nix { };
-        };
-      };
-    };
-  };
-
-  pkgs = import <nixpkgs> { inherit config; };
+  merkle-schemes-higher = pkgs.haskellPackages.callPackage ./merkle-schemes-higher/default.nix { };
 
 in
-  { merkle-schemes-higher = pkgs.haskellPackages.merkle-schemes-higher;
-    merkle-schemes-higher-bt = pkgs.haskellPackages.callPackage ./merkle-schemes-higher-bt/default.nix { };
-    merkle-schemes-higher-ext = pkgs.haskellPackages.callPackage ./merkle-schemes-higher-ext/default.nix { };
+  { merkle-schemes-higher = merkle-schemes-higher;
+    merkle-schemes-higher-bt = pkgs.haskellPackages.callPackage ./merkle-schemes-higher-bt/default.nix { merkle-schemes-higher = merkle-schemes-higher; };
+    merkle-schemes-higher-ext = pkgs.haskellPackages.callPackage ./merkle-schemes-higher-ext/default.nix { merkle-schemes-higher = merkle-schemes-higher; };
   }
 
 
